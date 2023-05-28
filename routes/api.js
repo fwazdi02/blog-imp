e1bae69e9476a0568c37ed24af602b06b0828059
Farid Wazdi <faridwazdi02@gmail.com>
2023-05-28 20:21:53 +0700

    fix: make public api/users, add api documentation

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-const userController = require("../controllers/UserController")
 const objectFileController = require("../controllers/ObjectFileController")
 const postController = require("../controllers/PostController")
 
-router.use("/users", userController)
 router.use("/file", objectFileController)
 router.use("/posts", postController)
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require("express")
 var router = express.Router()
+const userController = require("../controllers/UserController")
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
     res.render("index", { title: "Express" })
 })
+router.use("/api/users", userController)
 
 module.exports = router
